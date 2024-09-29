a947587899ad3ffb2cff4c2d03d572402a37d033
RC_Student_lab <RC_Student_lab@10.0.0.13>
2024-09-29 21:59:28 +0000

    second commit

--- src/main/java/com/mycompany/loginsystem/register.java
+++ src/main/java/com/mycompany/loginsystem/register.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.loginsystem;
+import java.util.Scanner;
 /**
  *
  * @author RC_Student_lab
@@ -16,5 +17,19 @@ public class register {
     String providedusername;
     String providedpassword;
     
+    Scanner scanner = new Scanner(System.in);
+
+       public boolean Checkusername(String username){
+   // varifying username
+   if(providedusername.contains("_") && providedusername.length() >=2 && providedusername.length() <=5){
+       System.out.println("Username secured");
+       return true;
+      }
+   else{
+       System.out.println("Unable to secure username please try again");
+       return false;
+      
+   }
+}
 }
     
